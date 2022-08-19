4cdab31831cb3862584f160e7c9343dfe935c30a
Pamplozio <giusepperosso@hotmail.com>
2022-08-19 20:42:27 +0100

    implement function for the button

---
+++ resources/main.js
@@ -0,0 +1,16 @@
+function appear() {
+    document.getElementById('htmlPic').style.display = 'block';
+
+    document.getElementById('cssPic').style.display = 'block';
+
+    document.getElementById('jsPic').style.display = 'block';
+
+    document.getElementById('gitHubPic').style.display = 'block';
+
+    document.getElementById('gitPic').style.display = 'block';
+
+    document.getElementById('excelPic').style.display = 'block';
+
+}
+
+
